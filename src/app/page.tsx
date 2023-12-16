6a34ed76331cb275afd1b664c1f8551705d8284c
Furkan <FSDeryol@gmail.com>
2023-12-16 21:23:19 +0300

    hangman words are added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 import React, { useState } from "react"
 import HangmanDrawing from "@/components/HangmanDrawing/HangmanDrawing"
+import HangmanWord from "@/components/HangmanWord/HangmanWord"
 import KeyboardGrid from "@/components/KeyboardGrid/KeyboardGrid"
+import words from "../../public/words.json"
 import styles from "../styles/homepage.module.scss"
 
 export default function Home() {
+  const index = Math.floor(Math.random() * words.commonWords.length)
+  const selectedWord = words.commonWords[index]
+  const selectedWordArray = selectedWord.split("")
+
   return (
     <div className={styles["homepage-container"]}>
-      <div className={styles["hangman-wrapper"]}>
-        <HangmanDrawing />
-      </div>
-      <div className={styles["buttons-wrapper"]}>
-        <KeyboardGrid />
-      </div>
+      <HangmanDrawing />
+      <HangmanWord word={selectedWordArray} />
+      <KeyboardGrid />
     </div>
   )
 }
