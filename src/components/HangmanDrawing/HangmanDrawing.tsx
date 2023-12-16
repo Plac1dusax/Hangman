0715ec19b2deecd62b168556764674fa1da6d5a9
Furkan <FSDeryol@gmail.com>
2023-12-16 19:46:27 +0300

    keyboard added

---
+++ src/components/HangmanDrawing/HangmanDrawing.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+import styles from "./hangmanDrawing.module.scss"
+
+export default function HangmanDrawing() {
+  return <div className={styles["hangman-drawing-wrapper"]}></div>
+}
